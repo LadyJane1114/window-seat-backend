5b9953880a7b05115464924afbd44961846916c8
Lady Jane <sweet.jane.s@outlook.com>
2026-04-10 14:21:38 -0300

    Was trying to get purchase/checkout connected, but couldn't figure it out. final commit

--- src/main/java/com/nscc/onlinestore/controller/CheckoutController.java
+++ src/main/java/com/nscc/onlinestore/controller/CheckoutController.java
@@ -3,8 +3,12 @@ package com.nscc.onlinestore.controller;
 
 import com.nscc.onlinestore.dto.CartDTO;
 import com.nscc.onlinestore.dto.CartItemDTO;
+import com.nscc.onlinestore.dto.LineItemCreateDTO;
+import com.nscc.onlinestore.dto.PurchaseCreateDTO;
 import com.nscc.onlinestore.entity.Product;
+import com.nscc.onlinestore.entity.Purchase;
 import com.nscc.onlinestore.service.ProductService;
+import com.nscc.onlinestore.service.PurchaseService;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
@@ -21,12 +25,14 @@ import java.util.HashMap;
 
 public class CheckoutController {
     private final ProductService productService;
+    private final PurchaseService purchaseService;
 
 
-    public CheckoutController(ProductService productService,@Value("${stripe.api.key}") String STRIPE_KEY){
-        this.productService = productService;
+    public CheckoutController(ProductService productService, @Value("${stripe.api.key}") String STRIPE_KEY, PurchaseService purchaseService){
 
         Stripe.apiKey = STRIPE_KEY;
+        this.purchaseService = purchaseService;
+        this.productService = productService;
     }
 
     //post mapping
@@ -83,5 +89,23 @@ public class CheckoutController {
         map.put("customer_email", session.getCustomerDetails().getEmail());
 
         return map;
+    }
+
+    //UPDATE HERE
+    @PostMapping("/confirmation")
+    public void confirmPayment(@RequestBody CartDTO cart) {
+        PurchaseCreateDTO purchaseDTO = new PurchaseCreateDTO();
+        Purchase purchase = purchaseService.createPurchase(purchaseDTO);
+
+        for (CartItemDTO item: cart.getItems()){
+            LineItemCreateDTO dto = new LineItemCreateDTO();
+            dto.setProductId(item.getItemID());
+            dto.setQuantity(item.getQuantity().intValue());
+
+            purchaseService.addLineItemToPurchase(purchase.getPurchaseID(), dto);
+        }
+        purchaseService.markPurchasePaid(purchase.getPurchaseID());
+        System.out.println("CONFIRMATION HIT");
+        System.out.println(cart);
     }
 }
